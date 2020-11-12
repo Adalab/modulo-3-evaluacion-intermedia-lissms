52c44f610aaf0ea9056afcfb02c371e5c265c2b4
lissms <felissms@gmail.com>
2020-11-12 09:11:15 +0100

    intials commit

---
+++ docs/static/js/main.f2b62d75.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpmy-task-list"]=this["webpackJsonpmy-task-list"]||[]).push([[0],{10:function(t,n,e){"use strict";e.r(n);var s=e(1),c=e(0),i=e.n(c),o=e(3),r=e.n(o),a=(e(9),function(){return Object(s.jsx)("div",{children:"hola mundo"})});r.a.render(Object(s.jsx)(i.a.StrictMode,{children:Object(s.jsx)(a,{})}),document.getElementById("root"))},9:function(t,n,e){}},[[10,1,2]]]);
+//# sourceMappingURL=main.f2b62d75.chunk.js.map
